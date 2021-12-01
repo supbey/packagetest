9ff091792b00aacf559459a2be1f42f971be7d8c
supbey <supbey@qq.com>
2021-12-01 19:52:33 +0800

    first commit

---
+++ src/PackagetestService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Supbey\Packagetest;
+
+
+
+class PackagetestService
+{
+    public function do() {
+        echo "This is test package ...";
+    }
+}
---
+++ src/PackagetestServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Supbey\Packagetest;
+
+
+use Illuminate\Support\ServiceProvider;
+use Supbey\Packagetest\PackagetestService;
+
+class PackagetestServiceProvider extends ServiceProvider 
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/packagetest.php' => config_path('packagetest.php'), // 发布配置文件到 laravel 的config 下
+        ]);
+    }
+
+    public function register()
+    {
+        $this->app->singleton('packagetest', function () {
+            return new PackagetestService();
+        });
+    }
+
+
+}
